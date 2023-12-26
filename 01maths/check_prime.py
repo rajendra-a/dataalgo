9e590ddd86e874d49edeb6783393e0a0815e799d
rajendra-a <rajendrareddy.akkala@gmail.com>
2023-12-26 12:41:58 +0530

    added check prime number

--- 01maths/check_prime.py
+++ 01maths/check_prime.py
@@ -12,3 +12,9 @@ print(is_prime(11))
 def isprime(n):
     if n == 1:
         return False
+    i = 2
+    while (i*i <= n):
+        if n%i == 0:
+            return False
+        i += 1
+    return True
