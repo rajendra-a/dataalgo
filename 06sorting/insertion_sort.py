135f6f5d39de07c546219039cbf87bea71610489
Rajendra <rajendrareddy.akkala@gmail.com>
2024-12-03 21:34:26 +0530

    insertion sort added

--- 06sorting/insertion_sort.py
+++ 06sorting/insertion_sort.py
@@ -1,18 +1,12 @@
-# Insertion sort is simple sorting algorithm that works similar to the way you sort playing cards in your hands
-# the array is virtually split into sorted and unsorted part. Values from unsorted part are picked and placed at 
-# correct position in the sorted part 
-
-def insertion_sort(l):
-    
-    for i in range(1, len(l)):
-        x = l[i]
-        j = i-1
-        
-        while j>=0 and x<l[j]:
-            l[j+1] = l[j]
-            j = j-1
-        
-        l[j+1] = x
-        
+# An array values to sort
+def insertion_sort(array):
+    n = len(array)
+    for i in range(1, n):
+        insert_index = i
+        current_value = array.pop(i)
+        for j in range(i-1, -1, -1):
+            if array[j] > current_value:
+                insert_index = j
+    array.insert(insert_index, current_value)
 
 print(insertion_sort([20, 5, 40, 60, 10, 30]))
