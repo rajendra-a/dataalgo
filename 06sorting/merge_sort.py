ab4190f8110d4183094c42f64609f591d027372c
Rajendra <rajendrareddy.akkala@gmail.com>
2024-12-24 12:54:34 +0000

    added merge sort

--- 06sorting/merge_sort.py
+++ 06sorting/merge_sort.py
@@ -1,108 +1,64 @@
-# The merge sort algorithm is algorithm that is based on the devide and conquer paradigm. In this algorithm, the array
-# is initially devided into two equal halves and then they are combined in a sorted manner
+# Merge sort algorithm is a devide and conquer that sorts an array by breaking down into smaller arrays, and then building the array back
+# together the correct way that it sorted
 
+# Devide
+# The Algorithm starts with breaking up the array smaller and smaller pieces untitll one such subarray only consists of one element
 
-# Merge sort working process:
-# Think of it as a recursive algorithm continuously splits the array in half until it cannnot be further devided.
-# This means that if the array becomes empty or has only one element left, the deviding will stop, i.e. it is the base 
-# case to stop recursion. If the array has multiple elements. split the array into halves and recursively invoke merge
-# sort on each of the halves. Finally when both halves are sorted, the merge operation is applied. Merge operation
-# merge opertion is the process of taking two smaller sorted arrays and combining them eventually make a larger one.
+# Conquer
+# The algorithm merges the small pieces of the array back together by putting the lowest value first, resulting in sorted array
 
-<<<<<<< HEAD
-def merge(arr, low, mid, high):
-    # creating left and right sub arrays
-    left = arr[low:mid + 1]
-    right = arr[mid + 1: high + 1]
-=======
+# The breaking down and building up of the array to sort the array is done recursively
+
 def merge(arr, start, mid, end):
-    # creating and intializing the left subarray and right sub arrays
-    # extra space - outplace sorting algorithm
+    # Create and intialize left and right sorted subarray
     left = arr[start:mid+1]
     right = arr[mid+1:end+1]
->>>>>>> f483e652a735d5d5b103600db9a9c271d3841686
     i = j = 0
     k = start
-    
+
     while i < len(left) and j < len(right):
         if left[i] < right[j]:
-<<<<<<< HEAD
             arr[k] = left[i]
-            
-            k += 1
-=======
-            arr[k] = left[i]
->>>>>>> f483e652a735d5d5b103600db9a9c271d3841686
             i += 1
-        else: 
-<<<<<<< HEAD
+        else:
             arr[k] = right[j]
-            
-            k += 1
-=======
-            arr[k] = right[j]
->>>>>>> f483e652a735d5d5b103600db9a9c271d3841686
             j += 1
-    k  += 1
-
-    # Copying the remaining elements from left subtree
-    while i < len(left):
-<<<<<<< HEAD
-        arr[k] = arr[i]
-        
-=======
-        arr[k] = arr[i]
->>>>>>> f483e652a735d5d5b103600db9a9c271d3841686
-        i += 1
         k += 1
 
-    # Copying the remaining elements from right subtree
+    # Copying remaining elements of left subarray
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+      
+
+    # Copying remaining element of right subarray
     while j < len(right):
-<<<<<<< HEAD
-        arr[k] = right[j] 
-        
-=======
-        arr[k] = right[j] 
->>>>>>> f483e652a735d5d5b103600db9a9c271d3841686
+        arr[k] = right[j]
         j += 1
         k += 1
         
-<<<<<<< HEAD
-def merge_sort(arr, low, high):
-    if low > high:
-        mid = (low+high)//2
-        merge_sort(arr, low, mid)
-        merge_sort(arr, mid+1, high)
-        merge(arr, low, mid, high)
-        
-=======
-def merge_sort(arr, start, end):
+
+
+# function definition of merge sort
+def mergesort(arr, start, end):
     if start < end:
-        # Devide
-        mid = start+(end-start)//2
-
-        # Conquer
-        # Recursive call for left subtree
-        merge_sort(arr, start, mid)
-        # Recursive call for right subtree
-        merge_sort(arr, mid+1, end)
-        # combine
-        # combine the left and right subtrees
+        mid = start + (end - start) // 2
+        mergesort(arr, start, mid)
+        mergesort(arr, mid+1, end)
         merge(arr, start, mid, end)
-        
-# Drivercode
->>>>>>> f483e652a735d5d5b103600db9a9c271d3841686
-arr = [10, 5, 30, 15, 7]
 
+
+
+arr = [2, 1, 4, 2, 3, 6, 8, 9, 4, 10, 11, 54, 23]
 start = 0
-
-end =len(arr) - 1
-
+end = len(arr) - 1 
 n = len(arr)
 
+for i in range(n):
+    print(arr[i], end=' ')
 
-# function calling
-merge_sort(arr, 0, 4)
+mergesort(arr, start, end)
 
-print(arr)
-
+for i in range(n):
+    print(arr[i], end=' ')
